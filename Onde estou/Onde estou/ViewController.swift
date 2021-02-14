0ce20360f1761bea851fc4871f1c22f352dcdc37
Gilberto Silva <gilberto.silva@sigga.com>
2021-02-13 21:16:48 -0300

    Criando layout do app.

---
+++ Onde estou/Onde estou/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Onde estou
+//
+//  Created by Gilberto da Luz on 13/02/21.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
