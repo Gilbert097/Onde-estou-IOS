715c335ce0da7dd81e14bc5c02f0069fbe20cdba
Gilberto Silva <betosilva.luz@gmail.com>
2021-02-14 16:44:31 -0300

    Exibindo detalhes do endereço do usuário.

--- Onde estou/Onde estou/ViewController.swift
+++ Onde estou/Onde estou/ViewController.swift
@@ -17,14 +17,17 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     @IBOutlet weak var addressLabel: UILabel!
     
     private var locationManager = CLLocationManager()
+    private let geocoder = CLGeocoder()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initLocationManager()
+    }
+    
+    private func initLocationManager() {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
-        
-        //initMap()
     }
     
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
@@ -37,7 +40,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
             print("Status: restricted")
         case .denied:
             print("Status: denied")
-            showRequestPermissionLocation()
+            showAlertRequestPermissionLocation()
         case .authorizedAlways:
             print("Status: authorizedAlways")
         case .authorizedWhenInUse:
@@ -48,57 +51,62 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         }
     }
     
-    private func showRequestPermissionLocation() {
-        let alertController = UIAlertController(title: "Permissão de localização",
-                                                message: "Necessário permissão para à sua localização!",
-                                                preferredStyle: .actionSheet)
-        let configurationAction = UIAlertAction(title: "Abrir configurações",
-                                                style: .default) { (action) in
-            
-            if let bundleId = Bundle.main.bundleIdentifier,
-               let url = URL(string: "\(UIApplication.openSettingsURLString)&path=LOCATION/\(bundleId)"), UIApplication.shared.canOpenURL(url)
-            {
-                UIApplication.shared.open(url, options: [:], completionHandler: nil)
-            }
-        }
-        
-        let cancelAction = UIAlertAction(title: "Cancelar", style: .default, handler: nil)
-        
-        alertController.addAction(configurationAction)
-        alertController.addAction(cancelAction)
-        present(alertController, animated: true, completion: nil)
-    }
-    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if  let location = locations.last{
-            let latitude: CLLocationDegrees = location.coordinate.latitude
-            let longitude: CLLocationDegrees = location.coordinate.longitude
-            
-            longitudeLabel.text = String(longitude)
-            latitudeLabel.text = String(latitude)
-            velocityLabel.text = String(location.speed)
-            
-            applyLocation(latitude: latitude, longitude: longitude)
+            applyLocationMap(location: location)
+            showLocationDetail(location)
         }
     }
     
-    private func initMap() {
-        let latitude: CLLocationDegrees = -19.90015864666512
-        let longitude: CLLocationDegrees = -44.08706408887549
+    private func showLocationDetail(_ location: CLLocation) {
+        longitudeLabel.text = String(location.coordinate.latitude)
+        latitudeLabel.text = String(location.coordinate.longitude)
+        velocityLabel.text = String(location.speed)
         
-        applyLocation(latitude: latitude, longitude: longitude)
-        addPointMyHouse(latitude, longitude)
+        showAddress(location)
     }
     
-    private func addPointMyHouse(_ latitude: CLLocationDegrees, _ longitude: CLLocationDegrees) {
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = CLLocationCoordinate2DMake(latitude, longitude)
-        annotation.title = "Casa"
-        annotation.subtitle = "Casa dos meus pais"
-        mapView.addAnnotation(annotation)
+    private func showAddress(_ location: CLLocation) {
+        geocoder.reverseGeocodeLocation(location) { (placemarks, error) in
+            if error == nil {
+                
+                if let placemarksRecived = placemarks,
+                   let placemarkFirst = placemarksRecived.first {
+                    
+                    let thoroughfare = placemarkFirst.thoroughfare ?? ""
+                    let subThoroughfare = placemarkFirst.subThoroughfare ?? ""
+                    let locality = placemarkFirst.locality ?? ""
+                    let subLocality = placemarkFirst.subLocality ?? ""
+                    let postalCode = placemarkFirst.postalCode ?? ""
+                    let country = placemarkFirst.country ?? ""
+                    let administrativeArea = placemarkFirst.administrativeArea ?? ""
+                    let subAdministrativeArea = placemarkFirst.subAdministrativeArea ?? ""
+                    
+                    let addressSingleText = "\(thoroughfare) - \(subThoroughfare) / \(locality) / \(country)"
+                    self.addressLabel.text = addressSingleText
+                    
+                    let addressText = "\n thoroughfare: \(thoroughfare)" +
+                        "\n subThoroughfare: \(subThoroughfare)" +
+                        "\n locality: \(locality)" +
+                        "\n subLocality: \(subLocality)" +
+                        "\n postalCode: \(postalCode)" +
+                        "\n country: \(country)" +
+                        "\n administrativeArea: \(administrativeArea)" +
+                        "\n subAdministrativeArea: \(subAdministrativeArea)"
+                    
+                    print(addressText)
+                }
+                
+                
+            }else{
+                print("Error: \(String(describing: error))")
+            }
+        }
     }
     
-    private func applyLocation(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+    private func applyLocationMap(location: CLLocation) {
+        let latitude: CLLocationDegrees = location.coordinate.latitude
+        let longitude: CLLocationDegrees = location.coordinate.longitude
         let deltaLatitude: CLLocationDegrees = 0.01
         let deltaLongitude: CLLocationDegrees = 0.01
         
@@ -109,6 +117,38 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         mapView.setRegion(region, animated: true)
     }
     
+    private func showAlertRequestPermissionLocation() {
+        let alertController = UIAlertController(title: "Permissão de localização",
+                                                message: "Necessário permissão para à sua localização!",
+                                                preferredStyle: .actionSheet)
+        addAlertActions(alertController)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    private func addAlertActions(_ alertController: UIAlertController) {
+        let configurationAction = UIAlertAction(title: "Abrir configurações",
+                                                style: .default) { (action) in
+            
+            if let bundleId = Bundle.main.bundleIdentifier,
+               let url = URL(string: "\(UIApplication.openSettingsURLString)&path=LOCATION/\(bundleId)"),
+               UIApplication.shared.canOpenURL(url)
+            {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .default, handler: nil)
+        
+        alertController.addAction(configurationAction)
+        alertController.addAction(cancelAction)
+    }
     
     
+    private func addPoint(_ latitude: CLLocationDegrees, _ longitude: CLLocationDegrees) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+        annotation.title = "Teste"
+        annotation.subtitle = "Teste description"
+        mapView.addAnnotation(annotation)
+    }
 }
